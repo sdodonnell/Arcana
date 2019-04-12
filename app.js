1c811fa0fcc6ec653324071e34a07815e1fd7c1f
Sam <sam.d.odonnell@gmail.com>
2019-04-12 15:39:25 -0700

    Add basic routes

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const bodyParser = require('body-parser');
 
-const app = express()
+
+const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => console.log("Connected to MongoDB!"))
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.use('/api/users', users)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
