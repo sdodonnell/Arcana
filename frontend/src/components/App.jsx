dcdae8fed4405f771bdcad49f16233b51b4d9361
Sam <sam.d.odonnell@gmail.com>
2019-04-16 15:05:20 -0700

    Add session actions and reducer

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+import Splash from './splash';
+
---
+++ frontend/src/components/splash.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function splash() {
+  return (
+    <div>
+      <h1>Arcana</h1>
+    </div>
+  )
+}
